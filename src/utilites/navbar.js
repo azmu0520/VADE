08bb57a05a68e38687f311d317123a237941410a
azmu0520 <azizbekbootcam@gmail.com>
2022-04-17 17:15:47 +0500

    set

---
+++ src/utilites/navbar.js
@@ -0,0 +1,24 @@
+import Generic from '../components/Generic';
+
+export const data = [
+  {
+    title: 'Home',
+    path: '/home',
+    Component: Generic,
+  },
+  {
+    title: 'Projects',
+    path: '/projects',
+    Component: Generic,
+  },
+  {
+    title: 'Localities',
+    path: '/localities',
+    Component: Generic,
+  },
+  {
+    title: 'Developers',
+    path: '/developers',
+    Component: Generic,
+  },
+];
