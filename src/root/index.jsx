35d3170d636f55f179cbb7286feea699042d1c6d
azmu0520 <azizbekbootcam@gmail.com>
2022-04-17 21:29:28 +0500

    done

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import { data } from '../utilites/navbar';
+import Home from '../components/Home';
 const Root = () => {
   return (
-    <div>Root</div>
-  )
-}
+    <div>
+      <Routes>
+        <Route element={<Navbar />}>
+          {data.map(({ id, Component, path }) => (
+            <Route key={id} path={path} element={<Component />} />
+          ))}
+        </Route>
+        <Route path='/' element={<Navigate to='/home' />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default Root
+export default Root;
--- src/utilites/navbar.js
+++ src/utilites/navbar.js
@@ -2,21 +2,25 @@ import Generic from '../components/Generic';
 
 export const data = [
   {
+    id: 1,
     title: 'Home',
     path: '/home',
     Component: Generic,
   },
   {
+    id: 2,
     title: 'Projects',
     path: '/projects',
     Component: Generic,
   },
   {
+    id: 3,
     title: 'Localities',
     path: '/localities',
     Component: Generic,
   },
   {
+    id: 4,
     title: 'Developers',
     path: '/developers',
     Component: Generic,
